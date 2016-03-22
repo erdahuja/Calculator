8cfe3205d0731a184e5ead667baadb06067aed39
erdahuja <er.d.ahuja@gmail.com>
2016-03-22 12:54:27 +0530

    Reset of text fields Function added

--- calclogics.js
+++ calclogics.js
@@ -4,6 +4,7 @@
 window.addEventListener("load",init);
 function init()
 {
+
 var buttonArray=document.getElementsByTagName("button");
 for(var i=0;i<buttonArray.length;i++)
 {
@@ -18,11 +19,25 @@ function doOperation() {
     var firstNo=document.getElementById("firstNo").value;
     var secondNo=document.getElementById("secondNo").value;
    var operation = this.getAttribute("data-operation");
-    var expression = firstNo + operation + secondNo;
-    console.log("expression is",expression);
-    result=eval(expression);
-    document.getElementById("result").innerHTML=result;
 
+
+    if (operation === "clear") {
+        var textArray = document.getElementsByTagName("input");
+        //console.log("reset");
+        //console.log(textArray);
+        for (var i = 0; i < textArray.length; i++) {
+            // console.log(document.getElementsByTagName("input")[i].value);
+            document.getElementsByTagName("input")[i].value = "";
+        }
+    }
+    else {
+
+
+        var expression = firstNo + operation + secondNo;
+        console.log("expression is", expression);
+        result = eval(expression);
+        document.getElementById("result").innerHTML = result;
+    }
     /*
      result = calcObject[this.getAttribute("data-operation")](firstNo,secondNo);
      if(this.innerHTML==="Add"){
